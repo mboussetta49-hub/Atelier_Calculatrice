120be40f5e0fe7972f55d0f151975d3416eded04
Malak Boussetta <mboussetta49@gmail.com>
2026-03-10 10:07:49 +0100

    Ajout du fichier calculatrice

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require("http");
+const server = http.createServer((req, res) => {
+  console.log("Requete recue !");
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Bravo ! Le serveur Node fonctionne sur Docker.\n");
+});
+server.listen(3000, "0.0.0.0", () => {
+  console.log("SERVEUR DEMARRE SUR LE PORT 3000");
+});
