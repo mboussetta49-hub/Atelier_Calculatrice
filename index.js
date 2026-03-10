627b5ca8dd7a2aec2fdd647fe7332a3275f26f8c
Malak Boussetta <mboussetta49@gmail.com>
2026-03-10 10:19:17 +0100

    Mon premier test automatique

--- index.js
+++ index.js
@@ -8,3 +8,4 @@ const server = http.createServer((req, res) => {
 server.listen(3000, "0.0.0.0", () => {
   console.log("SERVEUR DEMARRE SUR LE PORT 3000");
 });
+console.log("Test automatique Jenkins OK");
